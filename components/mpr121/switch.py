8f61d3ea8d0fd8e7652c7d61ecdb7589f1df217d
Maxim Sharenkov <mcshaman@gmail.com>
2023-03-15 04:00:01 +0300

    Next

--- components/mpr121/switch.py
+++ components/mpr121/switch.py
@@ -21,7 +21,7 @@ MPR121Switch = mpr121_ns.class_("MPR121Switch", switch.Switch)
 
 def validate_supports(value):
     num = value[CONF_CHANNEL]
-    mode = value[CONF_NODE] if CONF_MODE in value else {}
+    mode = value[CONF_MODE] if CONF_MODE in value else {}
     is_highside = mode[CONF_HIGHSIDE] if CONF_HIGHSIDE in mode else False
     is_lowside = mode[CONF_LOWSIDE] if CONF_LOWSIDE in mode else False    
 
